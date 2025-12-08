f0cd990c91f7fb07cb7d91edc567032e2219ec70
marhiru <marcosgamern@gmail.com>
2025-12-08 00:04:17 -0300

    fix: optimized template

--- mio.h
+++ mio.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <cstddef>
 #include <cstdio>
 #include <ostream>
 #include <string_view>
@@ -7,12 +8,15 @@
 #ifndef MIO_H
 #define MIO_H
 
-#define println(...) ::mio::mprintln::builtin(__VA_ARGS__)
-
 namespace mio::mprintln {
-template <typename... T> int writer(const char *c, T &&...args) {
+template <typename... T> int writer(std::string_view bw, T &&...args) {
+  const char *c = bw.data();
+
   if constexpr (sizeof...(T) != 0) {
     const int w = std::printf(c, std::forward<T>(args)...);
+    if (w < 0) {
+      return -1;
+    }
     std::putchar('\n');
     return w + 1;
   }
@@ -20,13 +24,19 @@ template <typename... T> int writer(const char *c, T &&...args) {
   return std::fputs(c, stdout) + std::putchar('\n');
 }
 
+template <typename... T> int builtin(std::string_view fmt, T &&...args) {
+  return writer(fmt, std::forward<T>(args)...) + 1;
+}
+
 template <typename... T> int builtin(const char *fmt, T &&...args) {
   return writer(fmt, std::forward<T>(args)...) + 1;
 }
-template <typename T> inline int builtin(const T &fmt) {
-  writer(std::data(fmt)) + 1;
-  return static_cast<int>(std::size(fmt)) + 1;
+template <typename T> int builtin(const T &fmt) {
+  return writer(std::data(fmt)) + 1;
 }
 
 } // namespace mio::mprintln
+
+#define println(...) ::mio::mprintln::builtin(__VA_ARGS__)
+
 #endif // MIO_H
